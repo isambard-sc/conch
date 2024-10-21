64a901c288cb49c3c62f4fd371f4dc3d5b0d3113
Matt Williams <matt.williams@bristol.ac.uk>
2024-10-21 10:52:47 +0100

    Initial docs

---
+++ docs/conf.py
@@ -0,0 +1,34 @@
+# SPDX-FileCopyrightText: © 2024 Matt Williams <matt.williams@bristol.ac.uk>
+# SPDX-License-Identifier: MIT
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import tomllib
+from pathlib import Path
+
+project = 'Conch'
+copyright = '2024, Matt Williams'
+author = 'Matt Williams'
+release = tomllib.loads((Path(__file__).resolve().parent.parent / "Cargo.toml").read_text())["package"]["version"]
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_show_sphinx = False
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+# html_static_path = ['_static']
